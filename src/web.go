1c82d5207347753f900416974667d2f2ea885cad
shaipe <shaipe@sina.com>
2019-02-18 22:23:46 +0800

    添加go语言开发规范,对web应用进行模板输出è½测试

--- src/web.go
+++ src/web.go
@@ -2,17 +2,98 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+/**
+定列TodoPage数据类型
+ */
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+/**
+直接输出文本内容
+ */
 func sayHello(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "我运行了")
 }
 
+/**
+	从文本模板中输出内容
+ */
+func renderTemplate(w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.ParseFiles("src/template/index.html")
 
+	if err != nil {
+		errStr := err.Error()
+
+		errStr += getCurrentDirectory()
+
+		http.Error(w, errStr, http.StatusInternalServerError)
+		return
+	}
+
+	data := TodoPageData{
+		PageTitle: "My TODO list",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+	tmpl.Execute(w, data)
+}
+
+/**
+	获取子目录路径
+ */
+func substr(s string, pos, length int) string {
+	runes := []rune(s)
+	l := pos + length
+	if l > len(runes) {
+		l = len(runes)
+	}
+	return string(runes[pos:l])
+}
+
+/**
+	获取指定目录的父级目录地址
+ */
+func getParentDirectory(dirctory string) string {
+	return substr(dirctory, 0, strings.LastIndex(dirctory, "/"))
+}
+
+/**
+	获取当前运行的目录地址
+ */
+func getCurrentDirectory() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.Replace(dir, "\\", "/", -1)
+}
+
+
+/**
+	运行主函数
+ */
 func main(){
+	// http.HandleFunc("/", sayHello)
 	http.HandleFunc("/", sayHello)
+	http.HandleFunc("/tpl", renderTemplate)
 	log.Println("我启动了")
 	err := http.ListenAndServe(":7000", nil)
 	if err !=nil{
